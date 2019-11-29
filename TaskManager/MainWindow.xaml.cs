d947ebfdc930cfd5b4a1c424936fc6645dd08ba2
arkonq@gmail.com <arkonq@gmail.com>
2019-11-29 21:47:32 +0600

    Add project files.

---
+++ TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+
+namespace TaskManager
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private ObservableCollection<ProcessClass> processes = new ObservableCollection<ProcessClass>();
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var processes = Process.GetProcesses();
+			//Process[] processList = Process.GetProcesses();
+			//foreach (var process in processList)
+			//{
+			//	processes.Add(new ProcessClass
+			//	{
+			//		Id = process.Id,
+			//		Name = process.ProcessName,
+			//		Priority = process.BasePriority,
+			//		CurrentMemoryUsage = process.WorkingSet64,
+			//		PeakMemoryUsage = process.PeakWorkingSet64,
+			//	});
+			//}
+
+			taskManager.ItemsSource = processes;
+		}
+
+		private void OnRowDeleted(object sender, KeyEventArgs e)
+		{
+			var currentRow = (DataGridRow)taskManager
+				.ItemContainerGenerator
+				.ContainerFromIndex(taskManager.SelectedIndex);
+
+			if (e.Key == Key.Delete && !currentRow.IsEditing)
+			{
+				var selectedItem = taskManager.SelectedItem;
+				;
+				if (selectedItem != null)
+				{
+					using (var process = selectedItem as Process)
+					{
+						process.Kill();
+					}
+
+					var processes = Process.GetProcesses();
+					taskManager.ItemsSource = processes;
+					MessageBox.Show("Процесс завершен");
+				}
+			}
+		}
+	}
+}
---
+++ TaskManager/ProcessClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager
+{
+	public class ProcessClass
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Priority { get; internal set; }
+		public long CurrentMemoryUsage { get; internal set; }
+		public long PeakMemoryUsage { get; internal set; }
+
+		public override string ToString()
+		{
+			return Id.ToString();
+		}
+	}
+}
